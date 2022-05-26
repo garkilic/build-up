134cb7b5a4fb50f666e5b5d9e8483c071ab859ed
Griffin Arkilic <garkilic@gmail.com>
2022-05-26 10:25:31 -0700

    Removing icons for now to be able to build project.

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,27 @@
 import React from "react"
 import '../style.css'
-import { FaQuestion } from "react-icons/fa";
-import { FaPaperPlane } from "react-icons/fa";
-import { FaRegGrin } from "react-icons/fa";
-
+// import { FaQuestion } from "react-icons/fa";
+// import { FaPaperPlane } from "react-icons/fa";
+// import { FaRegGrin } from "react-icons/fa";
 
 
 export default function About() {
     return (
         <div className="containers container" id='about'>
             <div className="container--one">
-                <FaQuestion className="containers--icon" />
+                {/* <FaQuestion className="containers--icon" /> */}
                 <h2>Tell Us Your Goals</h2>
                 <p>Answer 10 simple questions that will help your personal trainer create the best workout plan for you. </p>
             </div>
 
             <div className="container--two">
-                <FaPaperPlane className="containers--icon"/>
+                {/* <FaPaperPlane className="containers--icon"/> */}
                 <h2>Receive Your Plan</h2>
                 <p>Within 48 hours, you will receive your custom workout plan via email.</p>
             </div>
 
             <div className="container--three">
-                <FaRegGrin className="containers--icon"/>
+                {/* <FaRegGrin className="containers--icon"/> */}
                 <h2>Follow Your Plan</h2>
                 <p>Follow your personalized plan and reap the benefits of a workout made for you.</p>
             </div>
