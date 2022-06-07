303f727388a570ae3c4658f292e0a096803e89c8
Griffin Arkilic <garkilic@gmail.com>
2022-06-07 14:25:37 -0700

    Button change

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,7 +30,7 @@ export default function Nav() {
 							</a>
 						</li>
 						<li>
-							<a className="navbar--signup" onClick={toggle} href="#">
+							<a className="navbar--signup" onClick={toggle}>
 								Sign Up
 							</a>
 						</li>
