0eeb5a17efeab31a2a9ea1192618795f3d640507
Griffin Arkilic <garkilic@gmail.com>
2022-06-07 10:11:50 -0700

    Modal test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,40 @@
-import React from "react"
-import "../style.css"
+import React from "react";
+import "../style.css";
+import { createPopup } from "@typeform/embed";
+import "@typeform/embed/build/css/popup.css";
+
+const { toggle } = createPopup("<nuT2Tr4W>");
 
 export default function Nav() {
-    return (
-        <header className="container container--flex">
-            <div className="navbar">
-                    <div>
-                        <h2 className="navbar--title">Build Up</h2>
-                    </div>
-                    <nav>
-                        <ul className="navbar--items">
-                            <li><a href="#about">About</a></li>
-                            <li><a href="#pricing">Pricing</a></li>
-                            <li><a href="mailto:tryagainlabs@gmail.com" target="_blank" rel="noopener noreferrer">Contact</a></li>
-                            <li className="navbar--signup"><a href="#about">Sign Up</a></li>
-                        </ul>
-                    </nav>
-            </div>
-        </header>
-            
-    )
-        
-    
-    
+	return (
+		<header className="container container--flex">
+			<div className="navbar">
+				<div>
+					<h2 className="navbar--title">Build Up</h2>
+				</div>
+				<nav>
+					<ul className="navbar--items">
+						<li>
+							<a href="#about">About</a>
+						</li>
+						<li>
+							<a href="#pricing">Pricing</a>
+						</li>
+						<li>
+							<a
+								href="mailto:tryagainlabs@gmail.com"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Contact
+							</a>
+						</li>
+						<li className="navbar--signup">
+							<a onClick={toggle}>Sign Up</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+	);
 }
-
