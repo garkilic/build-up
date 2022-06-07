9ff33ce6d03218a8cf819def08112b10dd87f7b8
Griffin Arkilic <garkilic@gmail.com>
2022-06-07 14:33:55 -0700

    Fixed button styling - Need to be responsive

--- src/components/Training.js
+++ src/components/Training.js
@@ -1,21 +1,27 @@
-import React from "react"
-import '../style.css'
+import React from "react";
+import "../style.css";
+import { createPopup } from "@typeform/embed";
+import "@typeform/embed/build/css/popup.css";
 
+const { toggle } = createPopup("<nuT2Tr4W>");
 
 export default function Training() {
-    return (
-        <section className=" training--section container">
-             <div className="training--pricing" id="pricing">
-             <h2>Early Bird Offering</h2>
-                <h3>Price: $75</h3>
-                <ul className="training--list">
-                    <li>6-month workout PDF customized to you.</li> <li>Time and science based workouts that fit your schedule</li>
-                    <li>No expensive apps or equipment needed.</li>
-                    <li>No monthly subscriptions.</li>
-                    <li>Team that cares about you.</li>             
-                </ul>
-                <a className="signup" href="#about">Try Now!</a>
-            </div> 
-        </section>
-    )
+	return (
+		<section className=" training--section container">
+			<div className="training--pricing" id="pricing">
+				<h2>Early Bird Offering</h2>
+				<h3>Price: $75</h3>
+				<ul className="training--list">
+					<li>6-month workout PDF customized to you.</li>{" "}
+					<li>Time and science based workouts that fit your schedule</li>
+					<li>No expensive apps or equipment needed.</li>
+					<li>No monthly subscriptions.</li>
+					<li>Team that cares about you.</li>
+				</ul>
+				<button className="signup" onClick={toggle}>
+					Try Now!
+				</button>
+			</div>
+		</section>
+	);
 }
