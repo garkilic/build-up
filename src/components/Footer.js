a0c495c149a107b1a97d06a4edf46b3b9d0fad01
Griffin Arkilic <garkilic@gmail.com>
2022-06-08 14:30:42 -0700

    Contact changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function Footer() {
-    return(
-        <div className="footer container" id="contact">
-            <h3>Try Again Labs 🧪</h3>
-                <ul className="footer--links">
-                    <li><a href="mailto:tryagainlabs@gmail.com">tryagainlabs@gmail.com</a></li>
-                </ul>
-        </div>
-    )
+	return (
+		<div className="footer container" id="contact">
+			<h3>Second Try Labs 🧪</h3>
+			<ul className="footer--links">
+				<li>
+					<a href="mailto:tryagainlabs@gmail.com">secondtrylabs@gmail.com</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
