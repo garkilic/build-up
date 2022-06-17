4e8c35bc3870894e24e8eabaa3951b6d7a76302d
Griffin Arkilic <garkilic@gmail.com>
2022-06-17 12:14:19 -0700

    Simple copy changes

--- src/components/Training.js
+++ src/components/Training.js
@@ -37,11 +37,11 @@ export default function Training() {
 				<h2>Early Bird Offering</h2>
 				<h3>Price: $75</h3>
 				<ul className="training--list">
-					<li>6-month workout PDF customized to you.</li>{" "}
+					<li>3-month workout PDF customized to you.</li>{" "}
 					<li>Time and science based workouts that fit your schedule</li>
 					<li>No expensive apps or equipment needed.</li>
 					<li>No monthly subscriptions.</li>
-					<li>Team that cares about you.</li>
+					<li>Simple Fitness.</li>
 				</ul>
 				<button className="signup" onClick={toggle}>
 					Try Now!
