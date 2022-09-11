17feb92fad11f1136c5cfc71271306b94c09c32a
Griffin Arkilic <garkilic@gmail.com>
2022-09-10 17:26:04 -0700

    Testing VS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
 	return (
 		<div className="footer container" id="contact">
-			<h3>Second Try Labs 🧪</h3>
+			<h3>Second Try Labs 🧪 Test</h3>
 			<ul className="footer--links">
 				<li>
 					<a href="mailto:tryagainlabs@gmail.com">secondtrylabs@gmail.com</a>
